43d375f8a507c5bc0efc85a58876a3ec772e6de8
Terrance <tblountcs@gmail.com>
2021-04-14 15:37:14 -0400

    adding test files

---
+++ test/com/algo/QuickSortTest.java
@@ -0,0 +1,10 @@
+package com.algo;
+
+import java.util.Random;
+
+//public class QuickSortTest {
+//}
+//
+//private int[] getRandomArray(int size, int lowBound, int highBound) {
+//    return  new Random().ints(size, lowBound, highBound).toArray();
+//}
